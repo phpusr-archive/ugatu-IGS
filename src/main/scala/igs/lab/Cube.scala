6a58e44ab9d6fd7759e78e32738d5fa3fc83dadb
phpusr <phpusr@gmail.com>
2014-04-11 19:10:57 +0600

    change: Refactoring Cube

--- src/main/scala/igs/lab/Cube.scala
+++ src/main/scala/igs/lab/Cube.scala
@@ -18,21 +18,17 @@ import scala.util.Random
  * Куб
  */
 class Cube(size: Double, color: Color, shade: Double) extends Group {
+  // Оси поворота объекта
   val rx = new Rotate(0,Rotate.X_AXIS)
   val ry = new Rotate(0,Rotate.Y_AXIS)
   val rz = new Rotate(0,Rotate.Z_AXIS)
+  getTransforms.addAll(rz, ry, rx)
 
-  // Кол-во сторон
-  val sidesNumber = 6
+  /** Кол-во сторон */
+  private val sidesNumber = 6
 
-  /** Генерирует случайны цвет */
-  def getRandomColor = () => {
-    def randomColorNumber = () => Random.nextInt(256)
-    Color.rgb(randomColorNumber(), randomColorNumber(), randomColorNumber())
-  }
-
-  // Цвета сторон объекта
-  val colors = if (color != null) {
+  /** Цвета сторон объекта */
+  private val colors = if (color != null) {
     for (i <- 1 to sidesNumber)
       yield color.deriveColor(0.0, 1.0, 1 - ((sidesNumber - i).toDouble / 10) * shade, 1.0)
   } else {
@@ -40,87 +36,97 @@ class Cube(size: Double, color: Color, shade: Double) extends Group {
     for(i <-1 to sidesNumber) yield getRandomColor()
   }
 
-  // back face
-  var index = 0
-  val r1 = RectangleBuilder.create()
-  r1.width(size)
-  r1.height(size)
-  r1.fill(colors(index))
-  r1.translateX(-0.5*size)
-  r1.translateY(-0.5*size)
-  r1.translateZ(0.5*size)
+  /** Список сторон объекта */
+  private var sidesList: ListBuffer[Rectangle] = null
 
-  // bottom face
-  index += 1
-  val r2 = RectangleBuilder.create()
-  r2.width(size)
-  r2.height(size)
-  r2.fill(colors(index))
-  r2.translateX(-0.5*size)
-  r2.translateY(0)
-  r2.rotationAxis(Rotate.X_AXIS)
-  r2.rotate(90)
+  // Инициализация объекта
+  init()
 
-  // right face
-  index += 1
-  val r3 = RectangleBuilder.create()
-  r3.width(size)
-  r3.height(size)
-  r3.fill(colors(index))
-  r3.translateX(-1*size)
-  r3.translateY(-0.5*size)
-  r3.rotationAxis(Rotate.Y_AXIS)
-  r3.rotate(90)
-
-  // left face
-  index += 1
-  val r4 = RectangleBuilder.create()
-  r4.width(size)
-  r4.height(size)
-  r4.fill(colors(index))
-  r4.translateX(0)
-  r4.translateY(-0.5*size)
-  r4.rotationAxis(Rotate.Y_AXIS)
-  r4.rotate(90)
-
-  // top face
-  index += 1
-  val r5 = RectangleBuilder.create()
-  r5.width(size)
-  r5.height(size)
-  r5.fill(colors(index))
-  r5.translateX(-0.5*size)
-  r5.translateY(-1*size)
-  r5.rotationAxis(Rotate.X_AXIS)
-  r5.rotate(90)
-
-  // top face
-  index += 1
-  val r6 = RectangleBuilder.create()
-  r6.width(size)
-  r6.height(size)
-  r6.fill(colors(index))
-  r6.translateX(-0.5*size)
-  r6.translateY(-0.5*size)
-  r6.translateZ(-0.5*size)
-
-  // Convert scala list => java list
-  val rectangleBuilders = ListBuffer(r1, r2, r3, r4, r5, r6)
-  val rectagleList = rectangleBuilders.map(_.build())
-  val rectagles: java.util.List[Rectangle] = {
-    JavaConversions.bufferAsJavaList(rectagleList)
+  /** Генерирует случайны цвет */
+  private def getRandomColor = () => {
+    def randomColorNumber = () => Random.nextInt(256)
+    Color.rgb(randomColorNumber(), randomColorNumber(), randomColorNumber())
   }
-
-  getTransforms.addAll(rz, ry, rx)
-  getChildren.addAll(rectagles)
 
   /** Меняет цвета сторон объекта */
   def changeColors() {
-    r1.build().setFill(getRandomColor())
-
-    rectagleList.foreach { el =>
+    sidesList.foreach { el =>
       el.setFill(getRandomColor())
     }
+  }
+
+  /** Инициализация объекта */
+  private def init() {
+    // back face
+    var index = 0
+    val r1 = RectangleBuilder.create()
+    r1.width(size)
+    r1.height(size)
+    r1.fill(colors(index))
+    r1.translateX(-0.5*size)
+    r1.translateY(-0.5*size)
+    r1.translateZ(0.5*size)
+
+    // bottom face
+    index += 1
+    val r2 = RectangleBuilder.create()
+    r2.width(size)
+    r2.height(size)
+    r2.fill(colors(index))
+    r2.translateX(-0.5*size)
+    r2.translateY(0)
+    r2.rotationAxis(Rotate.X_AXIS)
+    r2.rotate(90)
+
+    // right face
+    index += 1
+    val r3 = RectangleBuilder.create()
+    r3.width(size)
+    r3.height(size)
+    r3.fill(colors(index))
+    r3.translateX(-1*size)
+    r3.translateY(-0.5*size)
+    r3.rotationAxis(Rotate.Y_AXIS)
+    r3.rotate(90)
+
+    // left face
+    index += 1
+    val r4 = RectangleBuilder.create()
+    r4.width(size)
+    r4.height(size)
+    r4.fill(colors(index))
+    r4.translateX(0)
+    r4.translateY(-0.5*size)
+    r4.rotationAxis(Rotate.Y_AXIS)
+    r4.rotate(90)
+
+    // top face
+    index += 1
+    val r5 = RectangleBuilder.create()
+    r5.width(size)
+    r5.height(size)
+    r5.fill(colors(index))
+    r5.translateX(-0.5*size)
+    r5.translateY(-1*size)
+    r5.rotationAxis(Rotate.X_AXIS)
+    r5.rotate(90)
+
+    // top face
+    index += 1
+    val r6 = RectangleBuilder.create()
+    r6.width(size)
+    r6.height(size)
+    r6.fill(colors(index))
+    r6.translateX(-0.5*size)
+    r6.translateY(-0.5*size)
+    r6.translateZ(-0.5*size)
+
+    // Convert scala list => java list
+    val rectangleBuilders = ListBuffer(r1, r2, r3, r4, r5, r6)
+    sidesList = rectangleBuilders.map(_.build())
+    val rectagles = JavaConversions.bufferAsJavaList(sidesList)
+
+    getChildren.addAll(rectagles)
   }
 
 }
