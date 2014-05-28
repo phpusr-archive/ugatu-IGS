97b03d7fc4d5ac0c458f02df9f6aea3c0c0ce33e
phpusr <phpusr@gmail.com>
2014-05-28 11:33:41 +0600

    add: Classes for Coursework

---
+++ src/main/scala/igs/coursework/Main.scala
@@ -0,0 +1,182 @@
+package igs.coursework
+
+import javafx.application.Platform
+import javafx.scene.{PerspectiveCamera, Scene}
+import javafx.scene.layout.{HBox, StackPane, VBox}
+import javafx.scene.paint.Color
+import javafx.embed.swing.JFXPanel
+import scala.swing.{FlowPanel, MainFrame, SimpleSwingApplication}
+import javafx.scene.control.{Slider, Label, Button}
+import javafx.beans.value.{ObservableValue, ChangeListener}
+import javafx.event.{ActionEvent, EventHandler}
+import javafx.geometry.{Insets, Pos}
+import java.awt.Dimension
+import igs.lab.shape.{RotateShape, Ball}
+import igs.lab.Util.AxisTitle
+import igs.coursework.shape.Cube
+
+/**
+ * @author phpusr
+ *         Date: 11.04.14
+ *         Time: 11:13
+ */
+
+/**
+ * TODO каркасная модель
+ */
+object Main extends SimpleSwingApplication {
+  // Размеры формы
+  private val FormWidth = 600
+  private val FormHeight = 400
+
+  // Размеры модели
+  private val ModelWidth = 400
+  private val ModelHeight = 300
+
+  /** Модель поверхности */
+  private val SizeModel = 100
+  private val ColorNum = 240
+  private val ColorModel = if (false) null else Color.rgb(ColorNum, ColorNum, ColorNum)
+  private val ShadeModel = 0.5
+  private val CubeModel = new Cube(SizeModel, ColorModel, ShadeModel)
+  private val BallModel = new Ball(50)
+  private var objectModel: RotateShape = CubeModel
+  private val jfx = new JFXPanel
+  /** Непрозрачность **/
+  private val OpaqueModel = true
+
+  /** Создание главной формы */
+  def top = new MainFrame {
+    contents = new FlowPanel {
+      peer.add(jfx)
+      jfx.setPreferredSize(new Dimension(FormWidth, FormHeight))
+    }
+    centerOnScreen()
+  }
+
+  /** Отложенный запуск создания Java FX компонентов */
+  Platform.runLater(new Runnable {
+    override def run() = createJavaFxComponents()
+  })
+
+  /** Создание Java FX компонентов */
+  private def createJavaFxComponents() {
+    val root = new VBox(20)
+    val scene = new Scene(root, ModelWidth, ModelHeight, OpaqueModel)
+    scene.setCamera(new PerspectiveCamera)
+    jfx.setScene(scene)
+
+    // Модель объекта
+    var objectModelPanel = getObjectModelPanel(ModelWidth, ModelHeight)
+    root.getChildren.add(objectModelPanel)
+
+    // Слайдеры
+    val slidersBox = new HBox(20)
+    root.getChildren.add(slidersBox)
+    slidersBox.setAlignment(Pos.CENTER)
+
+    val sliderBoxX = createSliderBox(AxisTitle.X)
+    slidersBox.getChildren.add(sliderBoxX)
+    val sliderBoxY = createSliderBox(AxisTitle.Y)
+    slidersBox.getChildren.add(sliderBoxY)
+    val sliderBoxZ = createSliderBox(AxisTitle.Z)
+    slidersBox.getChildren.add(sliderBoxZ)
+
+    // Кнопки
+    val buttosnBox = new HBox(20)
+    root.getChildren.add(buttosnBox)
+    buttosnBox.setAlignment(Pos.CENTER_RIGHT)
+    buttosnBox.setPadding(new Insets(20, 20, 20, 20))
+
+    //  Смена фигуры
+    val changeObjectButton = new Button("Change shape")
+    buttosnBox.getChildren.add(changeObjectButton)
+    changeObjectButton.setOnAction(new EventHandler[ActionEvent] {
+      override def handle(event: ActionEvent) {
+        // Удаление панели со старой фигурой
+        root.getChildren.remove(objectModelPanel)
+        // Смена фигуры
+        objectModel match {
+          case cube: Cube => objectModel = BallModel
+          case ball: Ball => objectModel = CubeModel
+        }
+        // Создание и добавление панели с новой фигурой
+        objectModelPanel = getObjectModelPanel(ModelWidth, ModelHeight)
+        root.getChildren.add(0, objectModelPanel)
+
+        // Сброс значений слайдеров
+        sliderBoxX.getChildren.get(1).asInstanceOf[Slider].setValue(0)
+        sliderBoxY.getChildren.get(1).asInstanceOf[Slider].setValue(0)
+        sliderBoxZ.getChildren.get(1).asInstanceOf[Slider].setValue(0)
+      }
+    })
+
+    //  Построение каркасной модели
+    val wireframeModelButton = new Button("Wireframe model")
+    buttosnBox.getChildren.add(wireframeModelButton)
+    wireframeModelButton.setOnAction(new EventHandler[ActionEvent] {
+      override def handle(event: ActionEvent) = objectModel.wireframeModel()
+    })
+
+    //  Смена цвета объекта
+    val changeColorsButton = new Button("Change colors")
+    buttosnBox.getChildren.add(changeColorsButton)
+    changeColorsButton.setOnAction(new EventHandler[ActionEvent] {
+      override def handle(event: ActionEvent) = objectModel.changeColors()
+    })
+
+    //  Выход из программы
+    val exitButton = new Button("Exit")
+    buttosnBox.getChildren.add(exitButton)
+    exitButton.setPrefSize(150, 40)
+    exitButton.setOnAction(new EventHandler[ActionEvent] {
+      override def handle(event: ActionEvent) = System.exit(0)
+    })
+
+  }
+
+  /** Создание панели с заголовком и ползунком */
+  private def createSliderBox(title: String) = {
+    // Slider
+    val minValue = 0
+    val maxValue = 360
+    val startValue = 0
+    val slider = new Slider(minValue, maxValue, startValue)
+
+    // Поворот объекта по оси при изменении положения ползунка
+    slider.valueProperty.addListener(new ChangeListener[Number]() {
+      override def changed(observableValue: ObservableValue[_ <: Number], oldValue: Number, newValue: Number) {
+        val rotate = title match {
+          case AxisTitle.X => objectModel.rx
+          case AxisTitle.Y => objectModel.ry
+          case AxisTitle.Z => objectModel.rz
+        }
+
+        rotate.setAngle(newValue.doubleValue)
+      }
+    })
+
+    // Label
+    val label = new Label(title)
+
+    // Box
+    val box = new HBox(10)
+    box.getChildren.addAll(label, slider)
+    box
+  }
+
+  /** Панель с каркасной моделью */
+  private def getObjectModelPanel(width: Int, height: Int) = {
+    // Установка повортов по всем осям в 0
+    objectModel.rx.setAngle(0)
+    objectModel.ry.setAngle(0)
+    objectModel.rz.setAngle(0)
+
+    val stackPane = new StackPane()
+    stackPane.setPrefSize(width, height)
+    stackPane.getChildren.add(objectModel)
+    
+    stackPane
+  }
+
+}
---
+++ src/main/scala/igs/coursework/shape/Cube.scala
@@ -0,0 +1,131 @@
+package igs.coursework.shape
+
+import javafx.scene.transform.Rotate
+import javafx.scene.paint.Color
+import javafx.scene.shape.{Rectangle, RectangleBuilder}
+import scala.collection.mutable.ListBuffer
+import scala.collection.JavaConversions
+import igs.lab.Util
+import igs.lab.shape.RotateShape
+
+/**
+ * @author phpusr
+ *         Date: 11.04.14
+ *         Time: 16:20
+ */
+
+/**
+ * Куб
+ */
+class Cube(size: Double, color: Color, shade: Double) extends RotateShape {
+
+  /** Кол-во сторон */
+  private val sidesNumber = 6
+
+  /** Цвета сторон объекта */
+  private val colors = if (color != null) {
+    for (i <- 1 to sidesNumber)
+      yield color.deriveColor(0.0, 1.0, 1 - ((sidesNumber - i).toDouble / 10) * shade, 1.0)
+  } else {
+    //List(Color.ORANGE, Color.FUCHSIA, Color.GREEN, Color.RED, Color.BLUE, Color.GOLD)
+    for(i <-1 to sidesNumber) yield Util.getRandomColor()
+  }
+
+  /** Список сторон объекта */
+  private var sidesList: ListBuffer[Rectangle] = null
+
+  // Инициализация объекта
+  init()
+
+  /** Меняет цвета сторон объекта */
+  def changeColors() {
+    sidesList.foreach { el =>
+      el.setFill(Util.getRandomColor())
+      el.setStrokeWidth(0)
+    }
+  }
+
+  /** Построение каркасной модели */
+  def wireframeModel() {
+    sidesList.foreach { el =>
+      el.setFill(VoidsColor)
+      el.setStroke(FrameColor)
+      el.setStrokeWidth(2)
+    }
+  }
+
+  /** Инициализация объекта */
+  private def init() {
+    // back face
+    var index = 0
+    val r1 = RectangleBuilder.create()
+    r1.width(size)
+    r1.height(size)
+    r1.fill(colors(index))
+    r1.translateX(-0.5*size)
+    r1.translateY(-0.5*size)
+    r1.translateZ(0.5*size)
+
+    // bottom face
+    index += 1
+    val r2 = RectangleBuilder.create()
+    r2.width(size)
+    r2.height(size)
+    r2.fill(colors(index))
+    r2.translateX(-0.5*size)
+    r2.translateY(0)
+    r2.rotationAxis(Rotate.X_AXIS)
+    r2.rotate(90)
+
+    // right face
+    index += 1
+    val r3 = RectangleBuilder.create()
+    r3.width(size)
+    r3.height(size)
+    r3.fill(colors(index))
+    r3.translateX(-1*size)
+    r3.translateY(-0.5*size)
+    r3.rotationAxis(Rotate.Y_AXIS)
+    r3.rotate(90)
+
+    // left face
+    index += 1
+    val r4 = RectangleBuilder.create()
+    r4.width(size)
+    r4.height(size)
+    r4.fill(colors(index))
+    r4.translateX(0)
+    r4.translateY(-0.5*size)
+    r4.rotationAxis(Rotate.Y_AXIS)
+    r4.rotate(90)
+
+    // top face
+    index += 1
+    val r5 = RectangleBuilder.create()
+    r5.width(size)
+    r5.height(size)
+    r5.fill(colors(index))
+    r5.translateX(-0.5*size)
+    r5.translateY(-1*size)
+    r5.rotationAxis(Rotate.X_AXIS)
+    r5.rotate(90)
+
+    // top face
+    index += 1
+    val r6 = RectangleBuilder.create()
+    r6.width(size)
+    r6.height(size)
+    r6.fill(colors(index))
+    r6.translateX(-0.5*size)
+    r6.translateY(-0.5*size)
+    r6.translateZ(-0.5*size)
+
+    // Convert scala list => java list
+    val rectangleBuilders = ListBuffer(r1, r2, r3, r4, r5, r6)
+    sidesList = rectangleBuilders.map(_.build())
+    val rectagles = JavaConversions.bufferAsJavaList(sidesList)
+
+    getChildren.addAll(rectagles)
+  }
+
+}
