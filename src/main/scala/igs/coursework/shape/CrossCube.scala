c9c3a9fc1afa27ca9f8bc3363be70056047e5ec5
phpusr <phpusr@gmail.com>
2014-05-28 18:26:03 +0600

    add: Отступы и точка вращения для CrossCube

--- src/main/scala/igs/coursework/shape/CrossCube.scala
+++ src/main/scala/igs/coursework/shape/CrossCube.scala
@@ -19,24 +19,36 @@ class CrossCube(startPoint: Point3D, size: Double, color: Color, shade: Double) 
   /** Размер частей */
   private val partSize = size
   /** Размер промежутков между частями */
-  private val splitSize = partSize / 3
+  private val indentSize = partSize / 3
+
+  // Отступы
+  private val sx = startPoint.getX
+  private val sy = startPoint.getY
+  private val sz = startPoint.getZ
 
   init()
 
   def init() {
 
     /** Построение креста с параметрами по умолчанию */
-    val newCross = (x: Double, y: Double, z: Double) => new Cross(new Point3D(x, y, z), partSize, color, shade)
+    val newCross = (x: Double, y: Double, z: Double) => {
+      val sPoint = new Point3D(x, y, z)
+      new Cross(sPoint, partSize, color, shade)
+    }
 
-    val frontCross = newCross(0, 0, 0)
-    val middle = newCross(0, 0, splitSize)
-    val backCross = newCross(0, 0, splitSize * 2)
-    val rightCross = newCross(splitSize * -2, 0, splitSize * 2)
+    val frontCross = newCross(sx, sy, sz)
+    val middle = newCross(sx, sy, sz + indentSize)
+    val backCross = newCross(sx, sy, sz + indentSize * 2)
+    val rightCross = newCross(-sz + indentSize * -2, sy, sx + indentSize * 2)
     rightCross.ry.setAngle(90)
-    val leftCross = newCross(0, 0, 0)
+    val leftCross = newCross(sz, sy, -sx)
     leftCross.ry.setAngle(-90)
 
     partList = ListBuffer(middle, backCross, rightCross, leftCross, frontCross)
     getChildren.addAll(JavaConversions.bufferAsJavaList(partList))
+
+    /** Точка вращения */
+    val pivot = new Cube(startPoint, 5, new Color(1, 0, 0, 1), shade)
+    getChildren.add(pivot)
   }
 }
